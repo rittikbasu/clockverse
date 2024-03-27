9880d2eef5ad103b64ec7e839a4f6a8e53f6f446
rittikbasu <irittik@gmail.com>
2024-03-28 03:46:23 +0530

    feat: add a dark overlay over the entire image + update favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import { TextGenerateEffect } from "@/components/text-generate-effect";
 
 export default function Home() {
@@ -60,39 +61,45 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center h-dvh w-dvw">
-      <div className="absolute top-12 md:top-36 left-1/2 transform -translate-x-1/2 z-50 md:text-6xl text-4xl">
-        {currentTime}
-      </div>
-      {data.imageUrl && (
-        <div className="relative w-full h-full">
-          <div className="absolute inset-0 bg-black opacity-60"></div>
-          <Image
-            src={data.imageUrl}
-            alt="Random Portrait"
-            layout="fill"
-            objectFit="cover"
-            className=""
-            priority
-          />
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-8 py-16 w-full flex justify-center items-center bg-gradient-to-r from-black/5 via-black/30 to-black/5">
-            <TextGenerateEffect words={data.poem} />
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <title>clockverse - a poem for every minute</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center h-dvh w-dvw">
+        <div className="absolute top-12 md:top-36 left-1/2 transform -translate-x-1/2 z-50 md:text-6xl text-4xl">
+          {currentTime}
+        </div>
+        {data.imageUrl && (
+          <div className="relative w-full h-full">
+            <div className="absolute inset-0 bg-gradient-to-t from-black/30 via-black/30 to-black/30 z-40"></div>
+            <Image
+              src={data.imageUrl}
+              alt="Random Portrait"
+              layout="fill"
+              objectFit="cover"
+              className=""
+              priority
+            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-8 py-16 w-full flex justify-center items-center z-40">
+              <TextGenerateEffect words={data.poem} />
+            </div>
           </div>
-        </div>
-      )}
-      {data.poet && (
-        <div className="absolute bottom-12 md:bottom-36 left-1/2 transform -translate-x-1/2 z-50 md:text-xl bg-gradient-to-r from-black/0 via-black/30 to-black/0 p-2 md:py-4 md:px-28 group">
-          <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-100/80 whitespace-nowrap italic group-hover:hidden">
-            in the style of {data.poet}
-          </span>
-          <Link
-            href="http://twitter.com/_rittik"
-            className="hidden group-hover:block text-orange-300 hover:text-orange-400 tracking-widest underline underline-offset-8"
-          >
-            by @_rittik
-          </Link>
-        </div>
-      )}
-    </div>
+        )}
+        {data.poet && (
+          <div className="absolute bottom-12 md:bottom-36 left-1/2 transform -translate-x-1/2 z-50 md:text-xl group">
+            <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-100/80 whitespace-nowrap italic group-hover:hidden">
+              in the style of {data.poet}
+            </span>
+            <Link
+              href="http://twitter.com/_rittik"
+              className="hidden group-hover:block text-orange-300 tracking-widest hover:underline underline-offset-8"
+            >
+              by @_rittik
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }

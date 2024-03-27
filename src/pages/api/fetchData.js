a6a7b347bc8b396d7a227899284876f626f44d38
rittikbasu <irittik@gmail.com>
2024-03-27 05:58:08 +0530

    feat: add redis caching + random poet selection

--- src/pages/api/fetchData.js
+++ src/pages/api/fetchData.js
@@ -1,14 +1,71 @@
 import OpenAI from "openai";
+import { Redis } from "@upstash/redis";
 
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
+const redis = new Redis({
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
+});
+
 export default async function handler(req, res) {
-  const { time } = req.query;
+  const { time, tz } = req.query;
+  const redisPoemKey = time + " " + tz;
+  const redisImageKey = new Date()
+    .toISOString()
+    .replace(/:\d{2}\.\d{3}Z$/, "Z");
   let imageUrl;
+  const poets = [
+    "sylvia plath",
+    "robert frost",
+    "emily dickinson",
+    "rabindranath tagore",
+    "william wordsworth",
+    "pablo neruda",
+    "william shakespeare",
+    "john keats",
+    "alfred lord tennyson",
+    "william blake",
+    "edgar allan poe",
+    "walt whitman",
+    "langston hughes",
+    "maya angelou",
+    "robert burns",
+    "william butler yeats",
+    "dylan thomas",
+    "william carlos williams",
+    "ezra pound",
+    "wallace stevens",
+    "t.s. eliot",
+    "e.e. cummings",
+    "sara teasdale",
+    "carl sandburg",
+    "edna st. vincent millay",
+    "lang leav",
+    "rupi kaur",
+    "atticus",
+    "nayyirah waheed",
+    "rumi",
+  ];
+
+  const cachedData = await redis.get(redisPoemKey);
+  if (cachedData) {
+    const { poem, poet } = cachedData;
+    const cachedImage = await redis.get(redisImageKey);
+    if (cachedImage) {
+      imageUrl = cachedImage;
+    } else {
+      imageUrl =
+        "https://images.unsplash.com/photo-1710976151734-72b48a6d66f4?q=80&w=2548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    res.status(200).json({ poem, poet, imageUrl });
+    return;
+  }
+
   try {
-    const response = await fetch(
+    const unsplashResponse = await fetch(
       "https://api.unsplash.com/photos/random?orientation=portrait&topics=M8jVbLbTRws",
       {
         headers: {
@@ -16,11 +73,11 @@ export default async function handler(req, res) {
         },
       }
     );
-    if (!response.ok) {
+    if (!unsplashResponse.ok) {
       throw new Error("Rate Limit Exceeded");
     }
 
-    const data = await response.json();
+    const data = await unsplashResponse.json();
     imageUrl = data.urls.full;
   } catch (error) {
     // console.error(error);
@@ -28,14 +85,12 @@ export default async function handler(req, res) {
       "https://images.unsplash.com/photo-1710976151734-72b48a6d66f4?q=80&w=2548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   }
 
-  // // console.log(imageUrl);
-
-  const poemResponse = await openai.chat.completions.create({
+  const poet = poets[Math.floor(Math.random() * poets.length)];
+  const openaiResponse = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
-        content:
-          "You are a poet that makes a beautiful and short poem, no more than 4 verses long in the style of Sylvia Plath with the current time by using the time in the first line of the poem, write the time in words in full and not as numerals. Note: use \n to separate the verses.",
+        content: `You are a poet that makes a beautiful and short poem, no more than 4 verses long in the style of ${poet} with the current time by using the time in the first line of the poem, write the time in words in full and not as numerals. Note: use \n to separate the verses.`,
       },
       {
         role: "user",
@@ -45,7 +100,12 @@ export default async function handler(req, res) {
     model: "gpt-3.5-turbo",
   });
 
-  const poem = poemResponse.choices[0].message.content;
+  const poem = openaiResponse.choices[0].message.content;
 
-  res.status(200).json({ poem, imageUrl });
+  await redis.set(redisPoemKey, JSON.stringify({ poem, poet }), {
+    ex: 60,
+  });
+  await redis.set(redisImageKey, imageUrl, { ex: 60 });
+
+  res.status(200).json({ poem, poet, imageUrl });
 }

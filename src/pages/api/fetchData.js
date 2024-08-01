7684b38d6582496439b57583eade3b2a556baeca
rittikbasu <irittik@gmail.com>
2024-08-01 23:54:46 +0530

    feat: replace openrouter w groq + increase retry delay to 5s

--- src/pages/api/fetchData.js
+++ src/pages/api/fetchData.js
@@ -35,8 +35,8 @@ const poets = [
 ];
 
 const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env["OPENROUTER_API_KEY"],
+  baseURL: "https://api.groq.com/openai/v1",
+  apiKey: process.env["GROQ_API_KEY"],
 });
 
 const redis = new Redis({
@@ -44,132 +44,145 @@ const redis = new Redis({
   token: process.env.REDIS_TOKEN,
 });
 
-async function fetchPoem(poet) {
-  const openaiResponse = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: `You are a poet who writes a beautiful poem in the style of ${poet}, that is precisely 4 lines long. Your poem can be inspired by something profound, fun, or anything else that moves you. The poem should stand alone, encapsulating its essence in just these four lines, no more, no less. Please generate the poem as a standalone piece of text, with no additional notes, explanations, symbols, or system messages included. Your focus should be on delivering this poem in its purest form, allowing the words alone to convey its depth and resonance.`,
-      },
-      {
-        role: "user",
-        content: `Write a poem that is ONLY 4 LINES LONG.`,
-      },
-    ],
-    model: "mistralai/mistral-7b-instruct:free",
-  });
+export const config = {
+  maxDuration: 20,
+};
 
-  let poem = openaiResponse.choices[0].message.content;
-  poem = poem.split("\n").slice(0, 4).join("\n");
-  return poem;
+async function fetchPoem(poet) {
+  try {
+    const openaiResponse = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `You are a poet who writes a beautiful poem in the style of ${poet}, that is precisely 4 lines long. Your poem can be inspired by something profound, fun, or anything else that moves you. The poem should stand alone, encapsulating its essence in just these four lines, no more, no less. Please generate the poem as a standalone piece of text, with no additional notes, explanations, symbols, or system messages included. Your focus should be on delivering this poem in its purest form, allowing the words alone to convey its depth and resonance.`,
+        },
+        {
+          role: "user",
+          content: `Write a poem that is ONLY 4 LINES LONG.`,
+        },
+      ],
+      model: "gemma2-9b-it",
+    });
+    let poem = openaiResponse.choices[0].message.content
+      .split("\n")
+      .slice(0, 4)
+      .join("\n");
+    return poem;
+  } catch (error) {
+    console.error("Error fetching poem:", error);
+    return null;
+  }
 }
 
 async function fetchImage() {
-  const unsplashResponse = await fetch(
-    "https://api.unsplash.com/photos/random?orientation=portrait&topics=M8jVbLbTRws",
-    {
-      headers: {
-        Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
-      },
+  try {
+    const unsplashResponse = await fetch(
+      "https://api.unsplash.com/photos/random?orientation=portrait&topics=M8jVbLbTRws",
+      {
+        headers: {
+          Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+        },
+      }
+    );
+    if (!unsplashResponse.ok) {
+      throw new Error("Rate Limit Exceeded");
     }
-  );
-  if (!unsplashResponse.ok) {
-    throw new Error("Rate Limit Exceeded");
+
+    const data = await unsplashResponse.json();
+    return {
+      imageUrl: data.urls.full,
+      blurhash: data.blur_hash,
+      imageAlt: data.alt_description,
+    };
+  } catch (error) {
+    console.error(error);
+    return { rateLimitExceeded: true };
   }
-
-  const data = await unsplashResponse.json();
-  return {
-    imageUrl: data.urls.full,
-    blurhash: data.blur_hash,
-    imageAlt: data.alt_description,
-  };
 }
 
-async function acquireLock(lockKey) {
-  const lock = await redis.setnx(lockKey, "locked", { ex: 10 });
-  return lock === 1;
-}
-
-async function releaseLock(lockKey) {
-  await redis.del(lockKey);
-}
-
-async function waitForDataOrLock(redisPoemKey, redisImageKey, lockKey) {
+async function waitForDataOrLock(redisKey, lockKey, currentTime) {
   let attempt = 0;
-  while (attempt < 20) {
-    const cachedPoem = await redis.get(redisPoemKey);
-    const cachedImage = await redis.get(redisImageKey);
-    if (cachedPoem && cachedImage) {
-      return {
-        cachedPoem: cachedPoem,
-        cachedImage: cachedImage,
-        lockAcquired: false,
-      };
+  const maxAttempts = 3;
+  const retryDelay = 5000; // 5 seconds
+
+  while (attempt < maxAttempts) {
+    const cachedData = await redis.hgetall(redisKey);
+    if (cachedData && cachedData.poem && cachedData.imageUrl) {
+      return { cachedData, lockAcquired: false };
     }
-    if (await acquireLock(lockKey)) {
-      return {
-        cachedPoem: cachedPoem ? cachedPoem : null,
-        cachedImage: cachedImage ? cachedImage : null,
-        lockAcquired: true,
-      };
+
+    const lock = await redis.set(lockKey, currentTime, { nx: true, ex: 10 });
+    if (lock === "OK") {
+      return { cachedData: null, lockAcquired: true };
     }
-    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    await new Promise((resolve) => setTimeout(resolve, retryDelay));
     attempt++;
   }
+
+  throw new Error(
+    "Failed to acquire lock or retrieve data after maximum attempts"
+  );
+}
+
+function getUTCTime() {
+  const now = new Date();
+  return `${now.getUTCHours()};${String(now.getUTCMinutes()).padStart(2, "0")}`;
 }
 
 export default async function handler(req, res) {
-  const currentMinute = new Date().getMinutes();
-  const redisPoemKey = `poem_${currentMinute}`;
-  const redisImageKey = `image_${currentMinute}`;
+  const currentTime = getUTCTime();
+  const currentMinute = new Date().getUTCMinutes();
+  const redisKey = `data_${currentMinute}`;
   const lockKey = `lock_${currentMinute}`;
 
   try {
-    const { cachedPoem, cachedImage, lockAcquired } = await waitForDataOrLock(
-      redisPoemKey,
-      redisImageKey,
-      lockKey
+    const { cachedData, lockAcquired } = await waitForDataOrLock(
+      redisKey,
+      lockKey,
+      currentTime
     );
 
-    let poem, poet, imageUrl, blurhash, imageAlt;
-
-    if (cachedPoem) {
-      ({ poem, poet } = cachedPoem);
-    }
-
-    if (cachedImage) {
-      ({ imageUrl, blurhash, imageAlt } = cachedImage);
+    if (cachedData && !lockAcquired) {
+      return res.status(200).json(cachedData);
     }
 
     if (lockAcquired) {
-      if (!cachedPoem) {
-        poet = poets[Math.floor(Math.random() * poets.length)];
-        poem = await fetchPoem(poet);
-        await redis.set(redisPoemKey, JSON.stringify({ poem, poet }), {
-          ex: 86400,
-        });
-      }
+      // Generate new data
+      const poet = poets[Math.floor(Math.random() * poets.length)];
+      const poem = await fetchPoem(poet);
+      const imageResult = await fetchImage();
 
-      if (!cachedImage) {
-        ({ imageUrl, blurhash, imageAlt } = await fetchImage());
-        await redis.set(
-          redisImageKey,
-          JSON.stringify({ imageUrl, blurhash, imageAlt }),
-          { ex: 60 }
-        );
+      if (poem && imageResult && !imageResult.rateLimitExceeded) {
+        const { imageUrl, blurhash, imageAlt } = imageResult;
+        const newData = { poem, poet, imageUrl, blurhash, imageAlt };
+
+        // Store new data in Redis
+        await redis.hset(redisKey, newData);
+        await redis.expire(redisKey, 60);
+        await redis.del(lockKey);
+
+        return res.status(200).json(newData);
+      } else {
+        await redis.del(lockKey);
+        throw new Error("Failed to generate poem or fetch image");
       }
-      await releaseLock(lockKey);
     }
 
-    if (!poem || !imageUrl) {
-      return res
-        .status(503)
-        .json({ error: "Failed to retrieve or generate necessary data." });
-    }
-
-    res.status(200).json({ poem, poet, imageUrl, blurhash, imageAlt });
+    // If we reach here, something unexpected happened
+    throw new Error("Unexpected state: neither cached data nor lock acquired");
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
+    if (
+      error instanceof UpstashError &&
+      error.message.includes("max daily request limit exceeded")
+    ) {
+      console.error("Upstash Daily Limit Reached:", error.message);
+      res.status(429).json({
+        error: "Service temporarily unavailable due to request limit.",
+      });
+    } else {
+      console.error("Unexpected error:", error);
+      res.status(500).json({ error: "An unexpected error occurred." });
+    }
   }
 }
